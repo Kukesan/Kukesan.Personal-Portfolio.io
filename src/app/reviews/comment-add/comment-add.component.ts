13cb19ea78e7f349eb29620b32203e6a2d73aa20
Kukesan <kkukesan@gmail.com>
2024-01-17 23:35:53 +0530

    comment responsive added

--- src/app/reviews/comment-add/comment-add.component.ts
+++ src/app/reviews/comment-add/comment-add.component.ts
@@ -7,7 +7,7 @@ import 'firebase/storage';
 @Component({
   selector: 'app-comment-add',
   templateUrl: './comment-add.component.html',
-  styleUrls: ['./comment-add.component.scss']
+  styleUrls: ['./comment-add.component.css']
 })
 export class CommentAddComponent implements OnInit {
 
